17704ec6d17728858def34a536aae10f70bf4b9f
Luis <Luis>
2019-06-08 13:51:34 -0500

    Crear 3 mensajes en index.js

--- index.js
+++ index.js
@@ -13,4 +13,9 @@ saludar("Hola mundo").then(msg =>{
     console.log("se termino el mensaje")
 }).catch(err=>{
     console.log(err)
-})
+});
+
+
+console.log("Mensaje 1")
+console.log("Mensaje 2")
+console.log("Mensaje 3")
